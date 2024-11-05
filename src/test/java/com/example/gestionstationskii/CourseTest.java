94d5653e9e20408effcee96b90938f900d1b621a
yassinekmn <yassine.kammoun@esprit.tn>
2024-11-05 17:20:55 +0100

    Test pipeline

--- src/test/java/com/example/gestionstationskii/CourseTest.java
+++ src/test/java/com/example/gestionstationskii/CourseTest.java
@@ -15,6 +15,7 @@ import org.junit.jupiter.api.TestMethodOrder;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.time.LocalDate;
@@ -25,6 +26,7 @@ import static org.junit.jupiter.api.Assertions.*;
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)  // Ensure ordered execution
+@ActiveProfiles("test")
      class CourseTest {
 
     @Autowired
