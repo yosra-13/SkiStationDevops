222d32c32f96829c4759758779ce0a05010a0537
sirinerebai <sirine.rebai@esprit.tn>
2024-11-12 17:34:30 +0100

    Test5

--- src/test/java/com/example/gestionstationskii/RegistrationTest.java
+++ src/test/java/com/example/gestionstationskii/RegistrationTest.java
@@ -8,11 +8,12 @@ import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.time.LocalDate;
 
 import static org.junit.jupiter.api.Assertions.*;
-
+@ActiveProfiles("test")
 @SpringBootTest
 class RegistrationTest {
 
